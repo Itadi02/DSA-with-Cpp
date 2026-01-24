effc172cf1dac32091de30a503fa0a3e2cfcb070
Itadi02 <adityashaw053@gmail.com>
2026-01-24 12:25:12 +0530

    977. Squares of a Sorted Array

---
+++ TwoPointers/square_sorted_array.cpp
@@ -0,0 +1,32 @@
+// 977. Squares of a Sorted Array
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        vector<int> res(nums.size(), 0);
+        int left = 0;
+        int right = nums.size() - 1;
+
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            if (abs(nums[left]) > abs(nums[right]))
+            {
+                res[i] = nums[left] * nums[left];
+                left++;
+            }
+            else
+            {
+                res[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return res;
+    }
+};
