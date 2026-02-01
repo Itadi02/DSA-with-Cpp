16ee65db4b40d554e1bf8508cb31c1d62ae5d4c3
Itadi02 <adityashaw053@gmail.com>
2026-02-01 19:23:50 +0530

    852. Peak Index in a Mountain Array

---
+++ BinarySearch/peak_idx_montain_arr.cpp
@@ -0,0 +1,24 @@
+
+// 852. Peak Index in a Mountain Array
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int st = 0, end = arr.size() - 1;
+        while (st < end)
+        {
+            int mid = st + (end - st) / 2;
+            ;
+            if (arr[mid] < arr[mid + 1])
+                st = mid + 1;
+            else
+                end = mid;
+        }
+        return end;
+    }
+};
