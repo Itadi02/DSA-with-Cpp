21c407e5fd501309062da1d2d3c109b43e8b9106
Itadi02 <adityashaw053@gmail.com>
2026-02-01 10:07:33 +0530

    28. Find the Index of the First Occurrence in a String

---
+++ TwoPointers/find_first_occurence.cpp
@@ -0,0 +1,14 @@
+// 28. Find the Index of the First Occurrence in a String
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        return haystack.find(needle);
+    }
+};
