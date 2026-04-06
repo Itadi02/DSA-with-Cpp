47405a1b1ed5c9fd106e883af94f8a7ef36a8ad2
Itadi02 <adityashaw053@gmail.com>
2026-04-06 19:44:29 +0530

    700. Search in a Binary Search Tree added

---
+++ Trees/search_in_bst.cpp
@@ -0,0 +1,31 @@
+// 700. Search in a Binary Search Tree
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    // TreeNode(int x) : val(x), lteft, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (root == NULL)
+            return root;
+        if (root->val == val)
+            return root;
+        if (root->val > val)
+            return searchBST(root->left, val);
+        else
+            return searchBST(root->right, val);
+    }
+};
