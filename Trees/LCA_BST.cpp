d1f6b3b65ec53d7e2c12aa26fbc940a93762fa34
Itadi02 <adityashaw053@gmail.com>
2026-04-07 19:26:01 +0530

    235. Lowest Common Ancestor of a Binary Search Tree added

---
+++ Trees/LCA_BST.cpp
@@ -0,0 +1,29 @@
+// 235. Lowest Common Ancestor of a Binary Search Tree
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    // TreeNode(int x) : val(x), lteft, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == NULL)
+            return root;
+        if (root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if (root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
+};
